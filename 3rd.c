8d3fafd1e3371705412c019dd0de61099778c913
shrushtikulthe-cloud <shrushtikulthe@gmail.com>
2025-10-03 08:26:23 +0530

    Create 3rd.c

---
+++ 3rd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() 
+{
+    int period = 40;
+    int duty = 25;
+
+    for(int i=0; i<duty; i++) printf("#");
+    for(int i=duty; i<period; i++) printf("-");
+    printf("
+");
+    return 0;
+}
