ad34c78a02f2ac871a1929602d07c85e79e6c166
shrushtikulthe-cloud <shrushtikulthe@gmail.com>
2025-10-03 08:52:20 +0530

    Create 32nd.c

---
+++ 32nd.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int period = 18;
+    int duty = 9;
+    for(int i=0; i<duty; i++) printf("#");
+    for(int i=duty; i<period; i++) printf("-");
+    printf("
+");
+    return 0;
+}
