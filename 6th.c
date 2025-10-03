9f223d84e8adfc30d243f50a4d6c91f4386125cb
shrushtikulthe-cloud <shrushtikulthe@gmail.com>
2025-10-03 08:33:10 +0530

    Create 6th.c

---
+++ 6th.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int period = 20;
+    int duty = 15;
+    for(int i=0; i<duty; i++) printf("#");
+    for(int i=duty; i<period; i++) printf("-");
+    printf("
+");
+    return 0;
+}
